3edb24e30f5f05d51a1cd3e167e2d7a7f692e5b8
Ross Mooney <ross@boomtownroi.com>
2015-10-05 17:06:10 -0400

    Sample chat app works

--- SocketSample/ViewController.swift
+++ SocketSample/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import Socket_IO_Client_Swift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chatView:UITextView!
+    @IBOutlet weak var sendButton:UIButton!
+    @IBOutlet weak var sendField:UITextField!
+    
+    let socket = SocketIOClient(socketURL: "localhost:3000")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        socketToMe()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func send() {
+//        self.socket.emit("chat message", self.sendField.text!)
+        self.socket.emit("chat message", withItems: [self.sendField.text!])
+    }
 
+    func socketToMe() {
+        socket.on("connect") {data, ack in
+            print("socket connected")
+        }
+        
+        self.socket.on("") {data, ack in
+            self.socket.emitWithAck("", data)(timeoutAfter: 0) { _ in }
+        }
+        
+        self.socket.on("chat message") { data, ack in
+            if let value = data.first as? String {
+                self.chatView.text?.appendContentsOf(value + "\n")
+            }
+        }
+        
+        self.socket.connect()
+    }
 }
 
